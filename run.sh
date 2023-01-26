99cb81da2d14decce616468348eec8478d4bb2b7
Joshua Pollick <39402728+Hypersycos@users.noreply.github.com>
2023-01-26 23:11:32 +0000

    Fixed run.sh, was missing -o for the alphabetic thesaurus

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 python3 main.py authorlist distance -d 3 -f "$1" -o thesaurus.txt
-python3 main.py authorlist distance -d 3 -f "$1" -a thesaurus_alphabetical.txt
+python3 main.py authorlist distance -d 3 -f "$1" -a -o thesaurus_alphabetical.txt
